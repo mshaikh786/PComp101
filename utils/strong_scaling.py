151946d33053ab55c2bdda2ba4e31288aae0ca9f
Mohsin Shaikh <shaima0d@cdl4.hpc.kaust.edu.sa>
2019-10-27 11:55:50 +0300

    stable versions of the scaling tests with larger problem sizes

--- utils/strong_scaling.py
+++ utils/strong_scaling.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 import subprocess as sb
 import os
+import math
 from matplotlib import pyplot as plt
 import matplotlib
 
 
 def submit_job(mpi_tasks):
     
-    #out = sb.run(['srun','--hint=nomultithread', 'perf-report', '-n', '%s' %mpi_tasks, '-o',
-    #              'mpi_strong_scaling_%s_tasks_%s.txt' %(mpi_tasks,os.getenv('SLURM_JOBID')),
-    #              '%s/bin/heat_mpi'%os.getenv('PWD')],stdout=sb.PIPE,stderr=sb.PIPE)
     out = sb.run(['sbatch', '-n', '%s' %mpi_tasks, 'jobscripts/strong_scaling.slurm'],stdout=sb.PIPE,stderr=sb.PIPE)
     text=str()
     for word in out.args:
@@ -35,8 +33,8 @@ def get_compute_time(fname):
     f.close()
     t_time=0.0
     for line in buf:
-       if "Total time" in line:
-            t_time = float(line.split()[2])
+       if "real" in line:
+            t_time = float(line.split()[1])
     return t_time
 
 def wait_for_all_jobs(jobids):
@@ -54,18 +52,41 @@ def get_scaling(mpi_tasks,jobids):
     wait_for_all_jobs(jobids)
     results=dict()
     for i in range(len(mpi_tasks)):
-        fname = "mpi_strong_scaling_%sp_%s.txt" %(mpi_tasks[i],jobids[i])        
-        results.update( {i:get_compute_time(fname)} )
+        fname = "strong_scaling.%s.out" %(jobids[i])        
+        results.update( {mpi_tasks[i]:get_compute_time(fname)} )
     return results
 
 def plot(results):
     matplotlib.use('Agg')
-    plt.plot(list(results.keys()),list(results.values()))
-    plt.savefig('Strong_scaling.png')
+    procs=list(results.keys())
+    T=list(results.values())
+    S=[T[0]/T[i] for i in range(len(T))]
+    T_ideal=[T[0]/i for i in procs]
+    S_ideal=[T_ideal[0]/T_ideal[i] for i in range(len(T_ideal))]
+    E=[S[i]/procs[i] for i in range(len(procs))]
+    
+    print(T)
+    print(S)
+    print(E)
+    print(procs)
+    
+    plt.figure(1)
+    plt.plot(procs,T,label='Compute time')
+    plt.savefig('Runtime.png')
+    plt.close()
+    plt.figure(2)
+    plt.plot(procs,S,label='S_real')
+    plt.plot(procs,S_ideal,label='S_ideal')
+    plt.savefig('Speedup.png')
+    plt.close()
+    
+    plt.figure(3)
+    plt.plot(procs,E,label='E')
+    plt.savefig('Efficiency.png')
+    plt.close()    
 
 if __name__ == '__main__':
     mpi_tasks=[2**x for x in range(0,8)]
     jobids=execute_tests(mpi_tasks)
-    print(jobids)
     results = get_scaling(mpi_tasks,jobids)
     plot(results)
